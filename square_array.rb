b92ff03e35919e8c0ea7674e1d9d5370751e0c50
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-15 22:05:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
+
+
+#new_numbers = [9,10,16,25]
+
+def square_array(array = [1,2,3])
   # your code here
+  array.each do |num|
+     num ** 2
+  end
 end
+square_array = square_array(new_numbers = [9,10,16,25])
+
+#def square_array(new_numbers)
+  #new_numbers.each do |num|
+    #num = num ** 2
+  #end
+#end
+#square_array(new_numbers)
